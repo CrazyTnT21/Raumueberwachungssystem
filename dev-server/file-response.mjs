01b1628b206f5ac85b261b11c7a281010c327797
CrazyTnT <CrazyTnT21@gmail.com>
2023-11-01 12:38:48 +0100

    start

---
+++ dev-server/file-response.mjs
@@ -0,0 +1,25 @@
+﻿export class FileResponse
+{
+    StatusCode;
+    Data;
+    Headers;
+
+    constructor(statusCode, data, headers)
+    {
+        this.StatusCode = statusCode;
+        this.Data = data;
+        this.Headers = headers;
+    }
+}
+
+export const contentTypeExtensions = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".json": "text/json",
+    ".svg": "image/svg+xml",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".ico": "image/x-icon",
+};
