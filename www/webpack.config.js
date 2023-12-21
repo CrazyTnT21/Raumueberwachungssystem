57cdb19d6cc66501ec8960e89a44aaadc3f37cad
CrazyTnT21 <crazytnt21@gmail.com>
2023-12-21 15:29:16 +0100

    Dev Server konfiguriert. 404 Fallback & Dateien mit dem gleichen Verzeichnisnamen werden nun als Index verwendet.

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 
 module.exports = {
     devServer: {
@@ -8,6 +9,15 @@ module.exports = {
                 extensions: ["html"],
             },
         },
+        historyApiFallback: {
+            rewrites: [
+                {
+                    from: /\//,
+                    to: context => customDirectoryIndex(context)
+                },
+            ],
+        },
+
         compress: true,
         port: 80,
     },
@@ -18,3 +28,15 @@ module.exports = {
     mode: "development",
     target: "web",
 };
+
+function customDirectoryIndex(context)
+{
+    const parts = context.parsedUrl.path.split("/");
+    const filePath = context.parsedUrl.path + parts[parts.length - 2] + ".html";
+
+    if (fs.existsSync(path.join(__dirname, "src", filePath)))
+    {
+        return filePath;
+    }
+    return "404.html";
+}
