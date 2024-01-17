f01dd6d4a98ccb955bf51e5e8db8a3428277a7b5
Skiyaad <83559875+Skiyaad@users.noreply.github.com>
2024-01-17 14:44:11 +0100

    Backend test

--- www/src/index.js
+++ www/src/index.js
@@ -4,23 +4,10 @@ const ctx = document.getElementById("myChart");
 const ctx2 = document.getElementById("myChart2");
 
 
-createChart(ctx)
-createChart(ctx2)
-function createChart(elemnt){
+function createChart(elemnt,data){
     new Chart(elemnt, {
         type: "line",
-        data: {
-            labels: ["1:00", "2:00", "3:00", "4:00", "5:00", "6:00"],
-            datasets: [{
-                label: "CO2 Gehalt (%)",
-                data: [1, 14, 14, 3, 7, 3],
-                borderWidth: 1,
-            },{
-                label: "Temperatur (Celsius)",
-                data: [4, 3, 3, 3, 2,1],
-                borderWidth: 1,
-            }],
-        },
+        data: data,
         options: {
             devicePixelRatio: 4,
             responsive: true,
@@ -35,4 +22,37 @@ function createChart(elemnt){
             },
         },
     });
+}
+run();
+async function run(){
+    try {
+const response = await fetch("http://192.168.43.9:3000/Luft");
+if (!response)
+return;
+    }
+    catch(e){
+        return;
+    }
+
+const result = await response.json();
+console.log(result.items)
+const data = {
+    labels: result.items.map(item => item.measured.split("T")[1].split("Z")[0]),
+    datasets:[ {
+        label: "CO2 Gehalt",
+       data: result.items.map(item => item.value),
+       borderWidth: 1}],
+}
+console.log(data)
+/*
+{
+            labels: ["1:00", "2:00", "3:00", "4:00", "5:00", "6:00"],
+            datasets: [{
+                label: "CO2 Gehalt (%)",
+                data: [1, 14, 14, 3, 7, 3],
+                borderWidth: 1,
+            }],
+        }
+*/
+createChart(ctx,data)
 }
