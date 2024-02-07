3176072ff276a7dffbd7bd700861087824be3875
CrazyTnT <CrazyTnT21@gmail.com>
2024-02-07 18:59:58 +0100

    Implementiert, dass Date Input Components mit Dates anstatt Strings arbeiten.

--- www/src/assets/components/app-date-input.js
+++ www/src/assets/components/app-date-input.js
@@ -12,6 +12,28 @@ export class AppDateInput extends AppInput
   {
     this.shadowRoot.querySelector("input").type = "date";
   }
+
+  get value()
+  {
+    const value = super.value;
+    if (!value)
+      return value;
+
+    return new Date(value);
+  }
+
+  set value(value)
+  {
+    if (!value)
+    {
+      super.value = value;
+      return;
+    }
+
+    const valueWithoutTimezone = new Date(value);
+    valueWithoutTimezone.setMinutes(value.getMinutes() - value.getTimezoneOffset());
+    super.value = valueWithoutTimezone.toISOString().slice(0, 10);
+  }
 }
 
 customElements.define("app-date-input", AppDateInput);
